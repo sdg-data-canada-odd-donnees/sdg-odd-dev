6ff18f4fdebc3e63080b8d0442891f2c2b1957ca
MaiaPelletier <44327546+MaiaPelletier@users.noreply.github.com>
2022-01-13 20:39:13 +0000

    Deploying to gh-pages from  @ 08dfe271b6664dafe1fe5fcab41007519c87b3ec 🚀

--- assets/js/sdg.js
+++ assets/js/sdg.js
@@ -2849,8 +2849,8 @@ var mapView = function () {
     $('.map').show();
     $('#map').sdgMap({
       indicatorId: indicatorId,
-      mapOptions: {"minZoom":5,"maxZoom":10,"tileURL":"","tileOptions":{"id":"","accessToken":"","attribution":""},"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#888888","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#111111","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
-      mapLayers: [],
+      mapOptions: {"minZoom":2,"maxZoom":6,"tileURL":"https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png","tileOptions":null,"colorRange":"chroma.brewer.BuGn","noValueColor":"#f0f0f0","styleNormal":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#deebf7","dashArray":""},"styleHighlighted":{"weight":1,"opacity":1,"fillOpacity":0.7,"color":"#3182bd","dashArray":""},"styleStatic":{"weight":2,"opacity":1,"fillOpacity":0,"color":"#172d44","dashArray":"5,5"}},
+      mapLayers: [{"min_zoom":2,"max_zoom":6,"subfolder":"regions","label":"Regions"}],
       precision: precision,
       decimalSeparator: decimalSeparator,
     });
